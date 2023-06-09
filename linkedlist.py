30a77757036c44f75ecb5157404d0b0ebd4c0525
Truc <trucdg@bu.edu>
2023-06-09 13:55:24 -0400

    add LinkedList class and its constructor

--- linkedlist.py
+++ linkedlist.py
@@ -2,3 +2,9 @@ class Node:
     def __init__(self, value = None):
         self.value = value
         self.next = next
+
+class LinkedList:
+    def __init__(self, head = None):
+        self.head = head
+
+    def 
