ad58710e6cc19ba74da7a3783a0054fe3b5674fb
Truc <trucdg@bu.edu>
2023-06-09 14:18:18 -0400

    fix bug in the Node class, and in the append method

--- linkedlist.py
+++ linkedlist.py
@@ -1,16 +1,18 @@
 class Node:
     def __init__(self, value = None):
         self.value = value
-        self.next = next
+        self.next = None
 
 class LinkedList:
     def __init__(self, head = None):
         self.head = head
 
-    def append(self, new_node):
+    def append(self, new_value):
         """ 
-        append the new node to the end of the linked list
+        append the new value to the end of the linked list
         """
+        new_node = Node(new_value)
+
         # if the llist is empty
         if self.head == None:
             self.head = new_node
@@ -45,4 +47,27 @@ class LinkedList:
                 prev.next = curr.next
             else:
                 print("Deletion Error: no node with given data found.")
+
+    def print(self):
+        current = self.head
+        s = ""
+
+        while current:
+            s += str(current.value) + "->"
+            current = current.next
+        
+        s += "None"
+
+        print(s)
+
+if __name__ == "__main__":
+    # create a linked list
+    ll = LinkedList()
+    ll.append(6)
+    ll.append(7)
+    ll.append(8)
+    ll.append(6)
+
+    ll.print()
+
 
